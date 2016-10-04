1b357fd94d6a3cb8b5d6532f498c558ccebd622b
wisdomJ <godlove4172@naver.com>
2016-10-04 15:24:24 +0900

    last

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+// index.js
+
+var express    = require("express");
+var mongoose   = require("mongoose");
+var bodyParser  = require("body-parser");
 var methodOverride = require("method-override");
 var app = express();
 
-//DB setting
-mongoose.connect(process.env.MONGO_DB);
+// DB setting
+mongoose.connect(process.env.MONGO_DB); // 1
 var db = mongoose.connection;
 db.once("open", function(){
-  console.log("DB connected");
+ console.log("DB connected");
 });
 db.on("error", function(err){
-  console.log("DB ERROR : ", err);
+ console.log("DB ERROR : ", err);
 });
 
-//Other setting
+// Other settings
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
-//Routes
+// Routes
 app.use("/", require("./routes/home"));
-
-//Port setting
+app.use("/posts", require("./routes/posts"));
+// Port setting
 app.listen(3000, function(){
-  console.log("server on!");
+ console.log("server on!");
 });
--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,14 @@
+// routes/home.js
+
 var express = require("express");
 var router = express.Router();
 
-//home
+// Home
 router.get("/", function(req, res){
-  res.render("home/welcome");
+ res.render("home/welcome");
 });
-router.get("/about", function(req, res) {
-  res.render("home/about");
+router.get("/about", function(req, res){
+ res.render("home/about");
 });
 
 module.exports = router;
