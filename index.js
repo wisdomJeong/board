1db909454706a82588d50ab0fe9cbee6ff8c46e8
wisdomJ <godlove4172@naver.com>
2016-10-04 16:12:13 +0900

    user registration/update/view

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.use(methodOverride("_method"));
 // Routes
 app.use("/", require("./routes/home"));
 app.use("/posts", require("./routes/posts"));
+app.use("/users", require("./routes/users"));
+
 // Port setting
 app.listen(3000, function(){
  console.log("server on!");
